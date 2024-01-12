325b21dcf184dd2d0543415af4d674fb74307b42
Andrew <804023@sylvaniastudents.org>
2024-01-12 10:19:12 -0500

    4.4.1

--- SpawnManager.cs
+++ SpawnManager.cs
@@ -9,9 +9,16 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave();
     }
 
+    void SpawnEnemyWave()
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
     private Vector3 GenerateSpawnPosition() {
         float spawnPosX = Random.Range(-spawnRange, spawnRange);
         float spawnPosZ = Random.Range(-spawnRange, spawnRange);
